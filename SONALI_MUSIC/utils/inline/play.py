92c0bf8115e79c281200c9256cbe32833b55988e
toxicpapa00 <gyaneshpatel605@gmail.com>
2025-11-19 15:10:19 +0530

    Update play.py

--- SONALI_MUSIC/utils/inline/play.py
+++ SONALI_MUSIC/utils/inline/play.py
@@ -64,8 +64,8 @@ def stream_markup_timer(_, chat_id, played, dur):
          InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
         ],
         [
-         InlineKeyboardButton(text="• ᴍαɢɪᴄ ✨ •", url="https://t.me/DJxBEAT_bot?start=_tgr_dj5byk43Y2E1"),
-         InlineKeyboardButton(text="• ɢɪғᴛ 💫 •", url="http://t.me/djxbeat_bot?startgroup"),
+         InlineKeyboardButton(text="• ᴍαɢɪᴄ ✨ •", url="https://t.me/ytxmusic_bot?start=_tgr_JCUTUrY0YjQ1"),
+         InlineKeyboardButton(text="• ɢɪғᴛ 💫 •", url="http://t.me/ytxmusic_bot?startgroup"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -75,8 +75,8 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-         InlineKeyboardButton(text="• ᴍαɢɪᴄ ✨ •", url="https://t.me/DJxBEAT_bot?start=_tgr_dj5byk43Y2E1"),
-         InlineKeyboardButton(text="• ɢɪғᴛ 💫 •", url="http://t.me/djxbeat_bot?startgroup"),
+         InlineKeyboardButton(text="• ᴍαɢɪᴄ ✨ •", url="https://t.me/ytxmusic_bot?start=_tgr_JCUTUrY0YjQ1"),
+         InlineKeyboardButton(text="• ɢɪғᴛ 💫 •", url="http://t.me/ytxmusic_bot?startgroup"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
