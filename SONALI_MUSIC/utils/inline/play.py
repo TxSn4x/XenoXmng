67aee9fb30a8bec3b579e354bf37339417b6da76
TxSn4x <txsn4x@gmail.com>
2026-01-19 00:28:50 +0530

    Update play.py

--- SONALI_MUSIC/utils/inline/play.py
+++ SONALI_MUSIC/utils/inline/play.py
@@ -1,8 +1,12 @@
 import math
 from config import SUPPORT_CHAT, OWNER_USERNAME
 from pyrogram.types import InlineKeyboardButton
-from SONALI_MUSIC import app
+from SONALI_MUSIC_MUSIC import app
 import config
+import math
+
+from pyrogram.types import InlineKeyboardButton
+
 from SONALI_MUSIC.utils.formatters import time_to_seconds
 
 
@@ -34,40 +38,41 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "🅚—————————"
+        bar = "─◉─────────"
     elif 10 < umm < 20:
-        bar = "—🅘————————"
+        bar = "──◉────────"
     elif 20 <= umm < 30:
-        bar = "——🅣———————"
+        bar = "───◉───────"
     elif 30 <= umm < 40:
-        bar = "———🅣︎——————"
+        bar = "────◉──────"
     elif 40 <= umm < 50:
-        bar = "————🅤—————"
+        bar = "─────◉─────"
     elif 50 <= umm < 60:
-        bar = "—————🅑︎————"
+        bar = "──────◉────"
     elif 60 <= umm < 70:
-        bar = "——————🅞︎———"
+        bar = "───────◉───"
     elif 70 <= umm < 80:
-        bar = "———————🅣︎——"
+        bar = "────────◉──"
     elif 80 <= umm < 95:
-        bar = "————————🅢︎—"
+        bar = "─────────◉─"   
     else:
-        bar = "🅚︎—————————"
+        bar = "──────────◉"
+        
     buttons = [
-                [
+        [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
         ],
-                [
-         InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
-        ],
         [
-         InlineKeyboardButton(text="• ᴍαɢɪᴄ ✨ •", url="https://t.me/Saregamamusics_bot?start=_tgr_JCUTUrY0YjQ1"),
-         InlineKeyboardButton(text="• ɢɪғᴛ 💫 •", url="http://t.me/Saregamamusics_bot?startgroup"),
-        ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],     
+         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
@@ -75,9 +80,12 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-         InlineKeyboardButton(text="• ᴍαɢɪᴄ ✨ •", url="https://t.me/Saregamamusics_bot?start=_tgr_JCUTUrY0YjQ1"),
-         InlineKeyboardButton(text="• ɢɪғᴛ 💫 •", url="http://t.me/Saregamamusics_bot?startgroup"),
-        ],
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+         ],        
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
@@ -88,11 +96,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"SonaPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"ALPHAPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"SonaPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"ALPHAPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
@@ -103,7 +111,6 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         ],
     ]
     return buttons
-
 
 
 def livestream_markup(_, videoid, user_id, mode, channel, fplay):
@@ -153,3 +160,4 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
         ],
     ]
     return buttons
+       
