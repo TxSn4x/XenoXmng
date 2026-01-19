ed85c063b08b1ecab4f70fc24ae2674800bee4d7
TxSn4x <txsn4x@gmail.com>
2026-01-20 00:29:34 +0530

    Update info.py

--- SONALI_MUSIC/plugins/mics/info.py
+++ SONALI_MUSIC/plugins/mics/info.py
@@ -11,7 +11,7 @@ from typing import Union, Optional
 
 EVAA = [
     [
-        InlineKeyboardButton(text="✙ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ✙", url=f"https://t.me/Candyy_music_robot?startgroup=true"),
+        InlineKeyboardButton(text="✙ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ✙", url=f"https://t.me/kurumiMusicRobot?startgroup=true"),
     ],
 ]
 
@@ -73,7 +73,7 @@ INFO_TEXT = """
 ❍ ᴅᴄ ɪᴅ ▷ {}
 ❍ ʙɪᴏ ▷ {}
 
-❖ ϻᴧᴅє ʙʏ  ➛ [˹ᴋɪʀᴛɪ ꭙ ʙᴏᴛѕ˼ 💞](https://t.me/lll_BADNAM_BABY_lll)
+❖ ϻᴧᴅє ʙʏ  ➛ [˹AɴɪWᴇʙ Bᴏᴛs˼](https://t.me/aniweb_bots)
 ▬▭▬▭▬▭▬▭▬▭▬▭▬▭▬▭
 
 """
