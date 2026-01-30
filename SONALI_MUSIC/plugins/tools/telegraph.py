641e69def5a7b70e7b6e0ba332eb23dd96cbea75
TxSn4x <txsn4x@gmail.com>
2026-01-30 18:47:01 +0530

    Update telegraph.py

--- SONALI_MUSIC/plugins/tools/telegraph.py
+++ SONALI_MUSIC/plugins/tools/telegraph.py
@@ -6,7 +6,7 @@ import requests
 
 
 def upload_file(file_path):
-    url = "https://catbox.moe/user/api.php"
+    url = "https://api.telegra.ph"
     data = {"reqtype": "fileupload", "json": "true"}
     files = {"fileToUpload": open(file_path, "rb")}
     response = requests.post(url, data=data, files=files)
