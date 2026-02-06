6d72bd03575e8af0cf6a4643d86ae46aab00a8c1
TxSn4x <txsn4x@gmail.com>
2026-02-06 12:53:49 +0530

    Update wishcute.py

--- SONALI_MUSIC/plugins/Yumi/wishcute.py
+++ SONALI_MUSIC/plugins/Yumi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from SONALI_MUSIC import app 
 
-SUPPORT_CHAT = "aniweb_nexus"
+SUPPORT_CHAT = "anime_helix"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
