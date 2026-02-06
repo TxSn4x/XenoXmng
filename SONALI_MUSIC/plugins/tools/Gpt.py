d958d0149f0d0a3369472519683104825241cfdd
TxSn4x <txsn4x@gmail.com>
2026-02-06 12:29:53 +0530

    Update Gpt.py

--- SONALI_MUSIC/plugins/tools/Gpt.py
+++ SONALI_MUSIC/plugins/tools/Gpt.py
@@ -36,7 +36,7 @@ async def chat_gpt(bot, message):
                     end_time = time.time()
                     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
                     await message.reply_text(
-                        f" {x}      ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛  @dramaX_view",
+                        f" {x}      ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛  @helix_bots",
                         parse_mode=ParseMode.MARKDOWN
                     )
                 else:
